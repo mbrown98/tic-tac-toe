01c421f55109c9259fbd174028b2dfbc116390cd
Matthew Brown <mbrown@geomarvel.com>
2022-01-01 20:46:58 -0500

    module resolver setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Home, Game } from "./screens";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Home, Game } from "@screens";
 
 export default function App() {
     return (
         <View style={styles.container}>
+            <Image source={require("@assets/icon.png")} />
             <Home />
             <Game />
         </View>
