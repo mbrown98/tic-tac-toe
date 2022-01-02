a19b1a702d5efd5e97f86e7f58b30f7830646897
Matthew Brown <mbrown@geomarvel.com>
2022-01-02 18:01:22 -0500

    loading google fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
-import { Home, Game } from "@screens";
+import {
+    useFonts,
+    DeliusUnicase_400Regular,
+    DeliusUnicase_700Bold
+} from "@expo-google-fonts/delius-unicase";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+    const [fontLoaded] = useFonts({ DeliusUnicase_700Bold, DeliusUnicase_400Regular });
+
+    if (!fontLoaded) return <AppLoading />;
+
     return (
         <View style={styles.container}>
-            <Image source={require("@assets/icon.png")} />
-            <Home />
-            <Game />
+            <Text style={{ fontSize: 25, fontFamily: "DeliusUnicase_400Regular" }}>
+                Hello World!
+            </Text>
         </View>
     );
 }
