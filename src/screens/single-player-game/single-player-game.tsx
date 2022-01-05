51ca5419fb84aa5b9c71403b1969409f1c1fe830
Matthew Brown <mbrown@geomarvel.com>
2022-01-04 21:40:57 -0500

    is game terminal functuion added

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -3,15 +3,13 @@ import { SafeAreaView } from "react-native";
 import { GradientBackground } from "@components";
 import styles from "./single-player-game.styles";
 import { Board } from "@components";
-import { printFormattedBoard, isEmpty, isFull, getAvailableMoves } from "@utils";
+import { printFormattedBoard, isEmpty, isFull, getAvailableMoves, isTerminal } from "@utils";
 import { BoardState } from "@utils";
 
 export default function Game(): React.ReactElement {
     const b: BoardState = ["x", "o", null, "x", "o", "x", "x", "o", null];
     printFormattedBoard(b);
-    console.log(isEmpty(b));
-    console.log(isFull(b));
-    console.log(getAvailableMoves(b));
+    console.log(isTerminal(b));
     return (
         <GradientBackground>
             <SafeAreaView style={styles.container}>
