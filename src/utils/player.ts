e2c5901e3c76931fa8eb0e6873c5d359a21c17a1
Matthew Brown <mbrown@geomarvel.com>
2022-01-08 17:33:59 -0500

    best move func complete

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -2,37 +2,61 @@ import { BoardState } from "./types";
 import { isTerminal, getAvailableMoves, printFormattedBoard } from "./board";
 
 export const getBestMove = (state: BoardState, maximizing: boolean, depth = 0): number => {
-    const terminalObject = isTerminal(state);
+    const childValues: { [key: string]: string } = {};
+    const getBestMoveRecursive = (state: BoardState, maximizing: boolean, depth = 0): number => {
+        const terminalObject = isTerminal(state);
 
-    // BASE CASE
-    if (terminalObject) {
-        if (terminalObject.winner === "x") {
-            return 100 - depth;
-        } else if (terminalObject.winner === "o") {
-            return -100 + depth;
+        // BASE CASE
+        if (terminalObject) {
+            if (terminalObject.winner === "x") {
+                return 100 - depth;
+            } else if (terminalObject.winner === "o") {
+                return -100 + depth;
+            }
+            return 0;
         }
-        return 0;
-    }
 
-    if (maximizing) {
-        let best = -100;
-        getAvailableMoves(state).forEach(index => {
-            const child: BoardState = [...state];
-            child[index] = "x";
-            const childValue = getBestMove(child, false, depth + 1);
-            best = Math.max(best, childValue);
-        });
-        return best;
-    }
+        if (maximizing) {
+            let best = -100;
+            getAvailableMoves(state).forEach(index => {
+                const child: BoardState = [...state];
+                child[index] = "x";
+                const childValue = getBestMoveRecursive(child, false, depth + 1);
+                best = Math.max(best, childValue);
+                if (depth === 0) {
+                    childValues[childValue] = childValues[childValue]
+                        ? `${childValues[childValue]}, ${index}`
+                        : `${index}`;
+                }
+            });
+            if (depth === 0) {
+                const arr = childValues[best].split(",");
+                const rand = Math.floor(Math.random() * arr.length);
+                return parseInt(arr[rand]);
+            }
+            return best;
+        }
 
-    if (!maximizing) {
-        let best = 100;
-        getAvailableMoves(state).forEach(index => {
-            const child: BoardState = [...state];
-            child[index] = "o";
-            const childValue = getBestMove(child, true, depth + 1);
-            best = Math.min(best, childValue);
-        });
-        return best;
-    }
+        if (!maximizing) {
+            let best = 100;
+            getAvailableMoves(state).forEach(index => {
+                const child: BoardState = [...state];
+                child[index] = "o";
+                const childValue = getBestMoveRecursive(child, true, depth + 1);
+                best = Math.min(best, childValue);
+                if (depth === 0) {
+                    childValues[childValue] = childValues[childValue]
+                        ? `${childValues[childValue]}, ${index}`
+                        : `${index}`;
+                }
+            });
+            if (depth === 0) {
+                const arr = childValues[best].split(",");
+                const rand = Math.floor(Math.random() * arr.length);
+                return parseInt(arr[rand]);
+            }
+            return best;
+        }
+    };
+    return getBestMoveRecursive(state, maximizing, depth);
 };
