27ce6408c16fa360d49a6aea0f6957e3d3cb8d45
Matthew Brown <mbrown@geomarvel.com>
2022-01-11 20:23:03 -0500

    added win, loss, and draw sounds/haptics

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native";
 import { GradientBackground } from "@components";
 import styles from "./single-player-game.styles";
 import { Board } from "@components";
-import { isEmpty, isTerminal } from "@utils";
+import { Cell, isEmpty, isTerminal } from "@utils";
 import { BoardState, getBestMove } from "@utils";
 import { Audio, Video } from "expo-av";
 import * as Haptics from "expo-haptics";
@@ -23,6 +23,10 @@ export default function Game(): React.ReactElement {
 
     const popSoundRef = useRef<Audio.Sound | null>(null);
     const pop2SoundRef = useRef<Audio.Sound | null>(null);
+
+    const winSoundRef = useRef<Audio.Sound | null>(null);
+    const lossSoundRef = useRef<Audio.Sound | null>(null);
+    const drawSoundRef = useRef<Audio.Sound | null>(null);
 
     const gameResult = isTerminal(state);
 
@@ -48,10 +52,47 @@ export default function Game(): React.ReactElement {
         setTurn("BOT");
     };
 
+    const getWinner = (winnerSymbol: Cell): "HUMAN" | "BOT" | "DRAW" => {
+        if (winnerSymbol === "x") {
+            return isHumanMaximizing ? "HUMAN" : "BOT";
+        }
+        if (winnerSymbol === "o") {
+            return isHumanMaximizing ? "BOT" : "HUMAN";
+        }
+        return "DRAW";
+    };
+
     useEffect(() => {
         if (gameResult) {
             // game is over
-            alert("Game Over");
+            const winner = getWinner(gameResult.winner);
+            if (winner === "HUMAN") {
+                try {
+                    winSoundRef.current?.replayAsync();
+                    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+                    alert("You won");
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            if (winner === "BOT") {
+                try {
+                    lossSoundRef.current?.replayAsync();
+                    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+                    alert("You Lost");
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            if (winner === "DRAW") {
+                try {
+                    drawSoundRef.current?.replayAsync();
+                    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
+                    alert("It's a draw");
+                } catch (error) {
+                    console.log(error);
+                }
+            }
         } else {
             if (turn === "BOT") {
                 if (isEmpty(state)) {
@@ -62,7 +103,7 @@ export default function Game(): React.ReactElement {
                     setIsHumanMaximizing(false);
                     setTurn("HUMAN");
                 } else {
-                    const best = getBestMove(state, !isHumanMaximizing, 0, 1);
+                    const best = getBestMove(state, !isHumanMaximizing, 0, -1);
                     insertCell(best, isHumanMaximizing ? "o" : "x");
                     setTurn("HUMAN");
                 }
@@ -74,17 +115,33 @@ export default function Game(): React.ReactElement {
         // load sounds
         const popSoundObject = new Audio.Sound();
         const pop2SoundObject = new Audio.Sound();
+        const winSoundObject = new Audio.Sound();
+        const lossSoundObject = new Audio.Sound();
+        const drawSoundObject = new Audio.Sound();
+
         const loadSounds = async () => {
             await popSoundObject.loadAsync(require("@assets/pop_1.wav"));
             popSoundRef.current = popSoundObject;
             await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
             pop2SoundRef.current = popSoundObject;
+
+            await winSoundObject.loadAsync(require("@assets/win.mp3"));
+            winSoundRef.current = winSoundObject;
+
+            await lossSoundObject.loadAsync(require("@assets/loss.mp3"));
+            lossSoundRef.current = lossSoundObject;
+
+            await drawSoundObject.loadAsync(require("@assets/draw.mp3"));
+            drawSoundRef.current = drawSoundObject;
         };
         loadSounds();
         return () => {
             // unload our sounds
             popSoundObject && popSoundObject.unloadAsync();
             pop2SoundObject && pop2SoundObject.unloadAsync();
+            winSoundObject && winSoundObject.unloadAsync();
+            lossSoundObject && lossSoundObject.unloadAsync();
+            drawSoundObject && drawSoundObject.unloadAsync();
         };
     }, []);
 
