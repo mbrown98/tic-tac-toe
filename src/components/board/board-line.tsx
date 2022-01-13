53449d87a95eb5fb87e8215a9ca7256d87efa4ae
Matthew Brown <mbrown@geomarvel.com>
2022-01-12 21:49:18 -0500

    stylign winning line component

--- src/components/board/board-line.tsx
+++ src/components/board/board-line.tsx
@@ -1,6 +1,27 @@
 import { BoardResult } from "@utils";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    line: {
+        position: "absolute",
+        backgroundColor: "#f03"
+    },
+    vLine: {
+        width: 2,
+        height: "100%"
+    },
+    hLine: {
+        width: "100%",
+        height: 2
+    },
+    dLine: {
+        width: 2,
+
+        top: 0,
+        left: "50%"
+    }
+});
 
 type BoardLineProps = {
     size: number;
@@ -8,18 +29,43 @@ type BoardLineProps = {
 };
 
 export default function BoardLine({ size, gameResult }: BoardLineProps): React.ReactElement {
+    const diagonalHeight = Math.sqrt(Math.pow(size, 2) + Math.pow(size, 2));
     return (
         <>
             {gameResult && gameResult.column && gameResult.direction === "V" && (
-                <View>Vertical Line</View>
+                <View
+                    style={[
+                        style.line,
+                        style.vLine,
+                        { left: `${33.3333 * gameResult.column - 16.6666}%` }
+                    ]}
+                ></View>
             )}
 
             {gameResult && gameResult.row && gameResult.direction === "H" && (
-                <View>Horizontal Line</View>
+                <View
+                    style={[
+                        style.line,
+                        style.hLine,
+                        { top: `${33.3333 * gameResult.row - 16.6666}%` }
+                    ]}
+                ></View>
             )}
 
             {gameResult && gameResult.diagonal && gameResult.direction === "D" && (
-                <View>Diag Line</View>
+                <View
+                    style={[
+                        style.line,
+                        style.dLine,
+                        {
+                            height: diagonalHeight,
+                            transform: [
+                                { translateY: -(diagonalHeight - size) / 2 },
+                                { rotateZ: gameResult.diagonal === "MAIN" ? "-45deg" : "45deg" }
+                            ]
+                        }
+                    ]}
+                ></View>
             )}
         </>
     );
