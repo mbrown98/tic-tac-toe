c2c9589543a2eb804d392d1f7e772a491ba28c8d
Matthew Brown <mbrown@geomarvel.com>
2022-01-19 20:10:54 -0500

    first text input on login screen

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { ScrollView, TextInput } from "react-native";
+import { GradientBackground } from "@components";
+import styles from "./login.styles";
+import { colors } from "@utils";
 
 export default function Login(): React.ReactElement {
     return (
-        <View>
-            <Text>Login</Text>
-        </View>
+        <GradientBackground>
+            <ScrollView contentContainerStyle={styles.container}>
+                <TextInput
+                    placeholder="Username"
+                    placeholderTextColor="#5d5379"
+                    style={{
+                        height: 50,
+                        width: "100%",
+                        borderBottomWidth: 1,
+                        borderColor: colors.lightGreen,
+                        backgroundColor: colors.purple,
+                        padding: 10,
+                        color: colors.lightGreen,
+                        fontFamily: "DeliusUnicase_400Regular"
+                    }}
+                />
+            </ScrollView>
+        </GradientBackground>
     );
 }
